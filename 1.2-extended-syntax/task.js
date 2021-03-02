3ee3b930ecf278bb26b5360cd97e2032675537f3
lofoo <lofootballer2@mail.ru>
2021-03-02 20:34:15 +0700

    Test

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,6 +1,22 @@
+'use strict';
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let d = b*b - 4 * a * c;
+
+    if (d < 0) {
+        return [];
+    }
+
+    if (d === 0) {
+        let x = (-b + Math.sqrt(d)) / (2 * a);
+        return [x];
+    }
+
+    if (d > 0) {
+        let resultOne = (-b + Math.sqrt(d)) / (2 * a);
+        let resultTwo = (-b - Math.sqrt(d)) / (2 * a);
+        return [resultOne, resultTwo];
+    }
+
 }
 
 function getAverageMark(marks){
