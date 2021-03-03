da42be3d58b6411c9738b039bff0ca6d4aba8e9f
lofoo <lofootballer2@mail.ru>
2021-03-04 00:33:55 +0700

    Complete first task data-types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,6 +1,34 @@
+'use strict';
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let numberPercent = Number(percent);
+    let numberContribution = Number(contribution);
+    let numberAmount = Number(amount);
+
+
+    if (isNaN(numberPercent) === true) {
+        return 'Параметр процент содержит неправильное значение "' + percent + '"';
+    } else if (isNaN(numberContribution) === true) {
+        return 'Параметр начальный взнос содержит неправильное значение "' + contribution + '"';
+    } else if (isNaN(numberAmount) === true) {
+        return 'Параметр общая стоимость содержит неправильное значение "' + amount + '"';
+    } else {
+        let sumCredit = numberAmount - numberContribution;  //Считаем сумму кредита. Тело кредита = S
+
+        let yearOfDate = date.getFullYear();  // Получаем год окончания кредита
+        let currentDate = new Date();
+        let currentYear = currentDate.getFullYear(); //Получаем Текущий год
+        let countMonth = (yearOfDate - currentYear) * 12 + date.getMonth() + 1; // Количество месяцев кредита   = n
+        let percentOfMonth = numberPercent / 100 / 12; // Месячный процент = P
+
+        let monthlyPayment = sumCredit * (percentOfMonth + percentOfMonth / ((Math.pow((1 + percentOfMonth), countMonth)) - 1)); // Ежемесячный платеж
+
+        let totalAmount = monthlyPayment * countMonth;
+
+        return totalAmount.toFixed(2);
+
+    }
+
+
 }
 
 function getGreeting(name) {
