8e016e226056b5d70f8dc7bfcdd7c3612d122c3d
lofoo <lofootballer2@mail.ru>
2021-03-30 01:02:37 +0700

    Fucking prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,4 +1,17 @@
+// 'use strict';
 //String.prototype.isPalindrome - для задачи №1
+
+function isPalindrome(str) {
+    this.isPalindrome = str;
+    let normalStr = str.toLowerCase();
+    let reversStr = normalStr.split('').reverse().join('');
+    if (normalStr === reversStr) {
+        return true;
+    }
+    return false;
+}
+
+String.prototype = isPalindrome;
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
@@ -10,19 +23,3 @@ function checkBirthday(birthday) {
     // return verdict
 }
 
-
-const predator = {
-    food: 'meat',
-    color: 'orange'
-}
-
-function Tiger(name) {
-    this.name = name;
-}
-
-Tiger.prototype = predator;
-
-const tiger = new Tiger('Vasya');
-
-console.log(tiger);
-// console.log(tiger.food);
