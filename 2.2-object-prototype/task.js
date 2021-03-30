33cc11e1e54624529bef2bfa129b7f16205a3e46
evgeny <evgeny.andryukhov@telebreeze.com>
2021-03-30 11:04:26 +0700

    Great Success First Task!!!

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,17 +1,13 @@
 // 'use strict';
-//String.prototype.isPalindrome - для задачи №1
-
-function isPalindrome(str) {
-    this.isPalindrome = str;
-    let normalStr = str.toLowerCase();
-    let reversStr = normalStr.split('').reverse().join('');
+String.prototype.isPalindrome = function () {
+    console.log(this);
+    let normalStr = this.toLowerCase().split(' ').join('');
+    let reversStr = normalStr.split(' ').reverse().join('');
     if (normalStr === reversStr) {
         return true;
     }
     return false;
 }
-
-String.prototype = isPalindrome;
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
