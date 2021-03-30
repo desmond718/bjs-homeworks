d45ec60e579bcc7b522df94ca1a0688ddcf1b4a6
evgeny <evgeny.andryukhov@telebreeze.com>
2021-03-30 13:08:38 +0700

    Great Success All Task 2.2!!!

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,8 +1,7 @@
-// 'use strict';
+'use strict';
 String.prototype.isPalindrome = function () {
-    console.log(this);
     let normalStr = this.toLowerCase().split(' ').join('');
-    let reversStr = normalStr.split(' ').reverse().join('');
+    let reversStr = normalStr.split('').reverse().join('');
     if (normalStr === reversStr) {
         return true;
     }
@@ -10,12 +9,28 @@ String.prototype.isPalindrome = function () {
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    if (marks.length === 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+    let average = sum / marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
 }
 
+
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = +(new Date());
+    let birthdayUser = +(new Date(birthday));
+    let diff = now - birthdayUser;
+    let age = (diff - (Math.trunc(diff / 31536000000 ) / 4 * (31622400000 - 31536000000))) / 31536000000;
+    console.log(age);
+    if (age > 18) {
+        return true;
+    }
+    return false;
 }
 
