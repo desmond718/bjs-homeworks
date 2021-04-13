df976c967d72a78cb7cae54f7be82bb996b23605
evgeny <evgeny.andryukhov@telebreeze.com>
2021-04-13 11:12:44 +0700

    Update first task in 3.2 task

---
+++ 3.2-array-proto-hocs/functions.js
@@ -0,0 +1,31 @@
+'use strict';
+const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
+
+function getNames () {
+    const namesWeapon = weapons.map(weapon => weapon.name);
+    return namesWeapon;
+}
+
+function getCountReliableWeapons (durability) {
+    const durabilityWeapon = weapons.filter(durabilityInArr => durabilityInArr.durability > durability);
+    return durabilityWeapon.length;
+}
+
+function hasReliableWeapons (durability) {
+    const hasReliable = weapons.some(durabilityInArr => durabilityInArr.durability > durability);
+    return hasReliable;
+}
+
+function getReliableWeaponsNames (durability) {
+    const getDurabilityWeapon = weapons.filter(durabilityInArr => durabilityInArr.durability > durability);
+    const getNamesWeapon = getDurabilityWeapon.map(item => item.name)
+    return getNamesWeapon;
+}
+
+function getTotalDamage () {
+    let totalDamage = 0;
+    weapons.forEach((damageWeapon) => {
+        totalDamage += damageWeapon.attack;
+    })
+    return totalDamage;
+}
