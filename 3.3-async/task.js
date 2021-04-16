eef25665a7ba091da798b1657dd3ae8be116454c
evgeny <evgeny.andryukhov@telebreeze.com>
2021-04-16 10:26:57 +0700

    Update first task in 3.2 task

--- 3.3-async/task.js
+++ 3.3-async/task.js
@@ -5,10 +5,32 @@
               this.timerId = id;
      }
 
-     addClock (time, callback, clockId = null) {
+     addClock(time, callback, clockId = null) {
          if (clockId === null)  throw new Error('Id not defined');
          if (clockId === this.timerId) return console.error('Id already exist');
-         this.alarmCollection.push({id: clockId, time: time, callback: callback});
+
+         return this.alarmCollection.push({id: clockId, time: time, callback: callback});
+     }
+
+     removeClock(clockId) {
+          let removeId = this.alarmCollection.findIndex(item => item.id === clockId);
+          if (removeId) {
+              delete this.alarmCollection[removeId];
+              return true;
+          }
+          return false;
+     }
+
+     getCurrentFormattedTime() {
+          let currentTime = new Date().toTimeString();
+          return currentTime.slice(0,5);
+     }
+
+     start() {
+          const checkClock = (alarm) => {
+              if (alarm === this.getCurrentFormattedTime()) return callback;
+          }
 
      }
+
  }
